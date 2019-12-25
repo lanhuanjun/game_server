2c4e60dc81fbf085a02c6213d87e70e639010c90
lanhuanjun <lanhuanjun@foxmail.com>
2019-12-25 21:43:37 +0800

    linux build third-party

--- build/start_svc.sh
+++ build/start_svc.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-BIN_PATH=bin
 
-cd $BIN_PATH
+function make_logs_dir()
+{
+    if [ ! -d $1 ];then
+        mkdir -p $1
+    fi
+}
 
-LOG_DIR="../logs/"
-LOG_SETTING="--alsologtostderr=true --log_dir=${LOG_DIR} --colorlogtostderr=true --max_log_size=512 --logbufsecs=1 --vmodule=info=2,error=3"
-UPDATE_TICK=-SERVICE_UPDATE_TICK=1000
-EXTEND_PARAM="${UPDATE_TICK} ${LOG_SETTING}"
+function start_svc()
+{
+    BIN_PATH=bin
+    LOG_DIR="../logs/"
+    UPDATE_TICK=1000
 
-echo "start param:${EXTEND_PARAM}"
-./svc_launch -SERVICE_TYPE=1 -SERVICE_ID=1 $EXTEND_PARAM
-cd ..
+    LOG_SETTING="--alsologtostderr=true --log_dir=${LOG_DIR} --colorlogtostderr=true --max_log_size=512 --logbufsecs=1 --vmodule=info=2,error=3"
+    EXTEND_PARAM="-SERVICE_UPDATE_TICK=${UPDATE_TICK} ${LOG_SETTING}"
+
+    
+    cd $BIN_PATH
+    make_logs_dir $LOG_DIR
+    echo "start param:${EXTEND_PARAM}"
+    ./svc_launch -SERVICE_TYPE=1 -SERVICE_ID=1 $EXTEND_PARAM
+    cd ..
+}
+
+start_svc
